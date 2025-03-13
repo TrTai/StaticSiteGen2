0b873f2a2a7244369caa03c60bc58208ee0bd4d4
TrTai <chaney.t@live.com>
2025-03-12 21:43:05 -0400

    modified public directory references to docs directory for Github Pages use. build.sh script for usage with ghpages

--- src/main.py
+++ src/main.py
@@ -4,13 +4,19 @@ import shutil
 import re
 from blockformatting import markdown_to_html_node
 from pathlib import Path
+from sys import argv
 
 def main():
+    global basepath
+    basepath = argv
+    if basepath == "":
+        basepath = "/"
     #newTextNode = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     #print(newTextNode.__repr__())
-    clear_public()
+    #clear_public()
+    clear_docs()
     copy_static_to_public()
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", "docs")
     #generate_page("content/index.md", "template.html", "public/index.html")
 
 def clear_public():
@@ -23,6 +29,17 @@ def clear_public():
         print("Public directory removed.")
     os.mkdir("public")
 
+def clear_docs():
+    print(os.listdir())
+    if "docs" in os.listdir():
+        print("Directory 'docs' already exists.")
+        absolutePathtoPublic = os.path.join(os.getcwd(), "docs")
+        print(f"Removing directory {absolutePathtoPublic}")
+        shutil.rmtree(absolutePathtoPublic)
+        print("docs directory removed.")
+    os.mkdir("docs")
+
+
 
 def copy_static_to_public():
         if os.path.isdir("static"):
@@ -34,13 +51,13 @@ def copy_from_dir(path):
     if os.path.isdir(path):
         print(f"{path} is a directory")
         if path != "static":
-            print(f"Creating subdirectory {path.replace("static", "public")}")
-            os.mkdir(path.replace("static", "public"))
+            print(f"Creating subdirectory {path.replace("static", "docs")}")
+            os.mkdir(path.replace("static", "docs"))
         for item in os.listdir(path):
             copy_from_dir(os.path.join(path, item))
     else:
         print(f"{path} is not a directory. Copying file to Public")
-        shutil.copy(path, path.replace("static", "public"))
+        shutil.copy(path, path.replace("static", "docs"))
 
 def extract_title(markdown):
     header = re.search(r"# (.*)", markdown).group().lstrip("#").strip()
@@ -55,7 +72,7 @@ def generate_page(from_path, template_path, dest_path):
     md = mdFile.read()
     templateFile = open(template_path, "r")
     template = templateFile.read()
-    outputFile = template.replace("{{ Title }}", extract_title(md)).replace("{{ Content }}", markdown_to_html_node(md).to_html())
+    outputFile = template.replace("{{ Title }}", extract_title(md)).replace("{{ Content }}", markdown_to_html_node(md).to_html()).replace("href=\"/", f"href=\"{basepath}").replace("src=\"/", f"src=\"{basepath}")
     print(f"writing out file to {dest_path}")
     if os.path.dirname(dest_path) != "":
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
